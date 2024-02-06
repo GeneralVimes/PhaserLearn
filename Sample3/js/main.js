ff5fcd3c6c740f4c226a3e7dff5f59d82884103c
GeneralVimes <aviglau@gmail.com>
2024-02-06 10:56:24 +0200

    basic events

--- Sample3/js/main.js
+++ Sample3/js/main.js
@@ -10,12 +10,32 @@ class Example extends Phaser.Scene
             this.load.image('TX_RED', 'assets/red.png');
         }
 
+		makeMovingLogo(lx,ly){
+			let ob = new MovingLogo(this,lx,ly,'TX_LOGO')
+			this.objects.push(ob)
+			this.add.existing(ob)
+
+			return ob
+		}
+
+        clearAllObjects (){
+			console.log("clearAllObjects")
+			//пробігаємося по об'єктах
+			for (let i=this.objects.length-1; i>=0; i--){
+				let ob = this.objects[i]
+				//прибираємо їх з відображення
+				ob.removeFromDisplayList()
+			}
+			//очищаємо список
+			this.objects.length=0;
+		}
+
         create ()
         {
-            this.add.image(400, 300, 'TX_SKY');
+            this.skyIm = this.add.image(400, 300, 'TX_SKY');
             //створюємо масив об'єктів
 			this.objects = []
-			for (let i=0; i<100; i++){
+			for (let i=0; i<1; i++){
 				//створили об'єкт
 				let ob = new MovingLogo(this,400-200+400*Math.random(),300-150+300*Math.random(),'TX_LOGO')
 				// додали в масив
@@ -23,6 +43,52 @@ class Example extends Phaser.Scene
 				// додали на сцену
 				this.add.existing(ob)
 			}
+
+			this.input.on('pointerdown',(pointer, objectsClicked)=>{
+				console.log("pointerdown", pointer, objectsClicked)
+				//давайте при натиснені на екра створювати нове рухоме лого
+				this.makeMovingLogo(pointer.downX, pointer.downY)
+			})
+
+			this.input.keyboard.on('keydown', (evt) => {
+				console.log("keydown", evt)
+				if (evt.code=="Space"){
+					this.clearAllObjects()
+				}
+			})
+
+			window.addEventListener("wheel",(evt) => {
+				console.log("wheel", evt)
+				this.skyIm.y+=evt.deltaY*0.1
+            })
+
+			/*
+            this.input
+                // .setTopOnly(false) // If you want to check if more than the top most hitbox was clicked
+                .on('pointerdown', (pointer, objectsClicked) => { 
+                    this.handleDown(pointer, objectsClicked)
+            })
+                .on('pointerup', (pointer, objectsClicked) => { 
+                    this.handleUp(pointer, objectsClicked) 
+            })
+                .on('pointermove', (pointer, objectsClicked) => { 
+                    this.handleMove(pointer, objectsClicked)
+            })
+                .on('pointerover', (pointer, objectsClicked) => { 
+                    this.handleHover(pointer, objectsClicked)
+            })
+                .on('gameout', (tm, evt) => { 
+                    this.handleOut(tm, evt)
+            })
+            this.input.keyboard.on('keydown', (evt) => {
+					// console.log(evt.code)
+                    this.handleKeyDown(evt)
+            })
+
+			window.addEventListener("wheel",(evt) => {
+					// console.log(evt.code)
+                    this.handleMouseWheel(evt)
+            })	*/		
         }
 
 		update(t,dt){
